3e4641de533425298a2c272cf9e2d8ce6b099b94
Logan Miller <loganmiller040402@gmail.com>
2025-07-01 17:55:41 -0700

    Added standard PICOTOOL path

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 export PICO_SDK_PATH=~/Documents/git/pico-sdk
 export PICOTOOL_PATH=~/Documents/git/pico-tool
+export PICOTOOL_FETCH_FROM_GIT_PATH=~/Documents/git/picotool
 
 # Create the build directory
 mkdir build
